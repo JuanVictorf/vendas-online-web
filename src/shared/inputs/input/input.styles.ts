0d049fd0bd600b544ac624de4a2e7b582c8f3770
Juan_Victor <estudantejuan0@gmail.com>
2023-06-24 16:07:23 -0300

    adicionando rotas no app

--- src/shared/inputs/input/input.styles.ts
+++ src/shared/inputs/input/input.styles.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
-import { Space, Typography } from "antd";
+import { Typography } from "antd";
 import styled from "styled-components";
 
-const { Text, Link } = Typography;
+const { Text } = Typography;
 
 export const BoxInput = styled.div`
   width: 100%;
